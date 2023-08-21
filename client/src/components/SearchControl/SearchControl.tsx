7608d145b75eb647231bc103185c4734748a7e43
Andrea-Agosta <andrea.agosta85@gmail.com>
2023-08-21 11:03:37 +0200

    style and define home page

--- client/src/components/SearchControl/SearchControl.tsx
+++ client/src/components/SearchControl/SearchControl.tsx
@@ -97,8 +97,8 @@ function SearchControl() {
   }, []);
 
   return (
-    <section className="p-5 flex bg-blue-dark gap-4 text-blue-lighter font-lato font-bold text-xl text-left">
-      <form className="w-full">
+    <form className="p-5 flex bg-blue-dark/90 gap-4 text-blue-lighter font-lato font-bold text-xl text-left">
+      <div className="w-full">
         <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
           {componentsName.map((component) => (
             <li key={component} className="mb-2 flex flex-col gap-1">
@@ -112,8 +112,8 @@ function SearchControl() {
             </li>
           ))}
         </ul>
-      </form>
-    </section>
+      </div>
+    </form>
   );
 }
 
--- client/src/views/Home/Home.tsx
+++ client/src/views/Home/Home.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import SearchControl from '../../components/SearchControl/SearchControl';
+import '../../styles/Home.css';
 
 function Home() {
   return (
-    <section className="App">
+    <section className="bg-image min-h-screen flex flex-col">
+      <h1 className="py-10 md:py-28 font-lato font-bold text-3xl md:text-7xl text-center text-white text-stroke">
+        EXPLORE YOUR DREAM!
+      </h1>
       <SearchControl />
     </section>
   );
